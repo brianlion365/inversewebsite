fabab3fb7dbc11bad9feddcb441355d8b35f8c48
brianlion365 <95258581+brianlion365@users.noreply.github.com>
2022-04-11 12:15:54 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+//The search bar needs to fix- This is where users can search for things on the website
+
+
+
+//The connect of the API- That display the price
+
+
+
+//Then the responsivness of the website
+
+
+
+//structure the HTML and CSS CODE 
+
+
+//This is an example of what it would display on the website
+
+//Now inna sense, I want to use JS do this part of the coding
+
+document.getElementById('apple-share-price').innerHTML="Apple Share Price";
+document.getElementById('google-share-price').innerHTML="Google Share Price";
+document.getElementById('facebook-share-price').innerHTML="Facebook Share Price";
+document.getElementById('netflix-share-price').innerHTML="Netflix Share Price";
